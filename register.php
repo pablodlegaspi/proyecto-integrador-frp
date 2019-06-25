b94b42e01085ae484a786de58878aab2b4ae71b3
pablodlegaspi <pablodlegaspi@gmail.com>
2019-06-25 16:31:34 -0300

    Cambié bastante el registro para que tenga campos con labels, sea responsive, contenga los campos solicitados en el sprint y quede mas o menos lindo. Hice las validaciones y persistencias para todos los campos, pero faltan cosas, todavia no está el save ni compara con usuarios existentes ni nada, solo validacion y persistencia.

--- register.php
+++ register.php
@@ -1,10 +1,31 @@
 <?php
 
+  require_once "registerValidation.php";
 
+  $countries = [
+    'ar' => 'Argentina',
+		'br' => 'Brasil',
+		'bo' => 'Bolivia',
+		'co' => 'Colombia',
+		'ch' => 'Chile',
+		'ec' => 'Ecuador',
+		'pe' => 'Perú',
+    'pa' => 'Paraguay',
+		've' => 'Venezuela'
+  ];
 
+  if ($_POST) {
 
+    $fullNameInPost = trim( ucwords($_POST["fullName"]) );
+    //ucwords hace mayúscula la primera letra de cada palabra.
+    $userNameInPost = trim($_POST["userName"]);
+    $emailInPost = trim($_POST["email"]);
 
- ?>
+    $errorsInRegister = validateRegister ();
+
+  }
+
+?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -35,13 +56,80 @@
       </section>
       <h3 class="crea-tu-cuenta">Creá tu Cuenta</h3>
       <div class="formulario">
-        <form action="" method="">
-          <input class="form" type="text" placeholder="Nombre">
-          <input class="form" type="text" placeholder="Apellido">
-          <input class="form" type="text" placeholder="Correo Electrónico">
-          <input class="form" type="text" placeholder="Nombre de usuario">
-          <input class="form" type="password" placeholder="Contraseña nueva">
-          <input class="boton" type="submit" name="" value="Registrarte">
+        <form action="" method="post" enctype="multipart/form-data">
+          <div class="form-element">
+            <label for="fullName"><b>Nombre Completo:</b></label>
+            <input class="form" type="text" name="fullName" id="fullName" value="<?= isset($fullNameInPost) ? $fullNameInPost : ""; ?>">
+          </div>
+          <?php if ( isset($errorsInRegister["inFullName"]) ) : ?>
+            <div class="register-alert">
+              <?= $errorsInRegister["inFullName"] ?>
+            </div>
+          <?php endif; ?>
+          <!-- <div class="register-alert">
+            <span>ALERTA!</span>
+          </div> -->
+          <div class="form-element">
+            <label for="userName"><b>Nombre de Usuario:</b></label>
+            <input class="form" type="text" name="userName" id="userName" value="<?= isset($userNameInPost) ? $userNameInPost : ""; ?>">
+          </div>
+          <?php if ( isset($errorsInRegister["inUserName"]) ) : ?>
+            <div class="register-alert">
+              <?= $errorsInRegister["inUserName"] ?>
+            </div>
+          <?php endif; ?>
+          <div class="form-element">
+            <label for="email"> <b>Correo Electrónico:</b> </label>
+            <input class="form" type="email" name="email" id="email" value="<?= isset($emailInPost) ? $emailInPost : ""; ?>">
+          </div>
+          <?php if ( isset($errorsInRegister["inEmail"]) ) : ?>
+            <div class="register-alert">
+              <?= $errorsInRegister["inEmail"] ?>
+            </div>
+          <?php endif; ?>
+          <div class="form-element">
+            <label for="password"> <b>Contraseña:</b> </label>
+            <input class="form" type="password" name="password" id="password">
+          </div>
+          <?php if ( isset($errorsInRegister["inPassword"]) ) : ?>
+            <div class="register-alert">
+              <?= $errorsInRegister["inPassword"] ?>
+            </div>
+          <?php endif; ?>
+          <div class="form-element">
+            <label for="rePassword"> <b>Repetir contraseña:</b> </label>
+            <input class="form" type="password" name="rePassword" id="rePassword">
+          </div>
+          <?php if ( isset($errorsInRegister["inRePassword"]) ) : ?>
+            <div class="register-alert">
+              <?= $errorsInRegister["inRePassword"] ?>
+            </div>
+          <?php endif; ?>
+          <div class="form-element">
+            <label class="country-label" for="country">
+              <b>País de Nacimiento:</b>
+              <select class="countries" name="country">
+                <option value="">Elegí un país</option>
+                <?php foreach ($countries as $code => $country): ?>
+                  <!-- no entiendo bien esto de selected, lo use como referencia del ejercicio de cookies y session pero no lo entiendo, seguro en la clase de hoy (25/6) lo veamos -->
+                  <option <?= isset($_POST["country"]) && $_POST["country"] == $code ? "selected" : "" ; ?> value="<?= $code ?>"> <?= $country ?> </option>
+                <?php endforeach; ?>
+              </select>
+            </label>
+          </div>
+          <div class="form-element">
+            <label class="profile-pic-label-container" for="">
+              <b>Imagen de perfil:</b>
+              <label class="profile-pic-label" for="profilePic">
+                <i class="fas fa-file-upload"></i>
+                Mi Archivo
+              </label>
+              <input class"form" type="file" name="profilePic" id="profilPic" value="">
+            </label>
+          </div>
+          <div class="form-element">
+            <input class="boton" type="submit" name="register" value="Registrarme">
+          </div>
         </form>
       </div>
       <p class="p-condiciones">Al registrarte, aceptas nuestras <a class="register-condiciones" href="tyc.html">Condiciones</a>, la <a class="register-condiciones" href="tyc.html">Política de datos </a> y
--- registerValidation.php
+++ registerValidation.php
@@ -1,7 +1,59 @@
 <?php
 
+function validateRegister() {
 
+  $fullName = trim( ucwords($_POST["fullName"]) );
+  $userName = $_POST["userName"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $rePassword = $_POST["rePassword"];
+  $country = $_POST["country"];
+  $profillePic = $_FILES["profilePic"];
+  $errors = [];
 
+  if ( empty($fullName) ) {
+    $errors["inFullName"] = "Por favor completá con tu nombre y apellido.";
+  } //me maté buscando una expresión regular ideal que acepte mucha variedad de caracteres pero que tambien haga que cada nombre o apellido empiece con mayusculas, ésta que encontré acepta muchos tipos de caracteres pero no exige mayúsculas en el principio. https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-name
+  //otra opción a considerar fue esta pero aceptaba números https://andrewwoods.net/blog/2018/name-validation-regex/
+  // tampoco logré exigir máximo un espacio entre cada palabra
+  elseif (!preg_match("/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/", $fullName)) {
+    $errors["inFullName"] = "Completar con el formato apropiado";
+  }
+
+  if ( empty($userName) ) {
+    $errors["inUserName"] = "Por favor completá con tu nombre de usuario.";
+  } //uso la misma regex que en el nombre completo pero agrego los números del 0 al 9, justo despues de A-Z y elimino el espacio al final de la lista de los caracteres raros, justo antes de la ",".
+  elseif (!preg_match("/^[a-zA-Z0-9àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð,.'-]+$/", $userName)) {
+    $errors["inUserName"] = "El nombre de usuario no puede contener espacios.";
+  }
+
+  if ( empty($email) ) {
+    $errors["inEmail"] = "Por favor completá con tu correo electrónico.";
+  } elseif ( !filter_var($email, FILTER_VALIDATE_EMAIL) ) {
+    $errors["inEmail"] = "Completar con el formato apropiado";
+  }
+
+  if ( empty($password) ) {
+    $errors["inPassword"] = "Por favor completá con tu contraseña.";
+  } elseif (strlen($password) < 5) {
+    $errors["inPassword"] = "La contraseña debe tener mínimo 5 caracteres.";
+  } elseif ( preg_match("/ /", $password) ) { //retorna true si hay un espacio, entonces:
+    $errors["inPassword"] = "La contraseña no puede contener espacios";
+  } elseif ( !preg_match("/DH/", $password) ) { //retorna true si contiene "DH", pero como tiene "!" al principio:
+    $errors["inPassword"] = "La contraseña debe incluir las letras 'DH'";
+  }
+
+  if ( empty($rePassword) ) {
+    $errors["inRePassword"] = "Por favor repetí tu contraseña.";
+  } elseif ($rePassword != $password) {
+    $errors["inRePassword"] = "Las contraseñas no coinciden";
+  }
+
+  
+
+  return $errors;
+
+}
 
 
 
